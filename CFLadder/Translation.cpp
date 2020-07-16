7dba72ae44d6b3dea9ec7d2de5b00fca45b44a2f
shubhu1603070 <shubhulol777@gmail.com>
2020-07-16 22:02:06 +0530

    Update

---
+++ CFLadder/Translation.cpp
@@ -0,0 +1,90 @@
+
+/* Shubhulol */
+
+//Header files
+#include 			<bits/stdc++.h>
+#include 			<ext/pb_ds/assoc_container.hpp>
+#include			<ext/pb_ds/tree_policy.hpp>
+#include 			<functional>
+
+
+
+//STL
+
+// typedef vector<int> vi;
+
+//Macros
+#define 			ll long long
+#define 			ld long double
+#define 			db double
+#define				str string
+#define             lli long long int
+#define 			ull unsigned long long
+#define 			f(i,x,n) for(ll int i=x;i<n;i++)
+#define 			re(j,x,n) for(ll int j=x;j>=n;j--)
+#define 			f1(k,x,n) for(ll int k=x;k<=n;k++)
+#define 			mp(x,y) std::make_pair(string(x),int(y))
+#define 			pb push_back
+#define 			input  file();
+#define 			bit(x, i) (x >>(i - 1) & 1ll)
+#define 			w() int x; cin>>x; while(x--)
+#define 			all(v) (v).begin(),(v).end()
+#define 			MAX 1000001
+#define 			nl '\n';
+#define  			c1(x) cin>>x;
+#define				c2(x,y) cin>>x>>y;
+#define 			c3(x,y,z) cin>>x>>y>>z;
+#define 			c4(a,b,c,d) cin>>a>>b>>c>>d;
+#define 			o1(x) cout<<x<<'\n';
+#define 			o2(x,y) cout<<x<<" "<<y<<'\n';
+#define 			o3(x,y,z) cout<<x<<" "<<y<<" "<<z<<'\n';
+#define 			o4(a,b,c,d) cout<<a<<" "<<b<<" "<<c<<" "<<d<<'\n';
+
+//Variables
+const ll MOD = 1e9 + 7;
+const ll maximum = 1000000;
+int inf = 0x3f3f3f3f;
+ll infl = 0x3f3f3f3f3f3f3f3fLL;
+ld infd = 1.0 / 0.0;
+
+//inclusion
+using namespace __gnu_pbds;
+using namespace std;
+
+//Unordered ds
+typedef tree<pair<ll int, ll int>, null_type, less<pair<ll int, ll int> >, rb_tree_tag, tree_order_statistics_node_update>
+new_data_set;
+
+//Input file
+void file() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#endif
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+
+
+int solve() {
+
+	str s;
+	getline(cin, s);
+	str s1;
+	getline(cin, s1);
+	reverse(s1.begin(), s1.end());
+	if (s == s1) {
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+	return 0;
+}
+
+
+
+int main()
+{
+	input
+	solve();
+	cout << endl;
+	return 0;
+}
